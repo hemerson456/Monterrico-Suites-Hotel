ae3f7fff82fdf698275ff8e13f44ff2e91531c72
hemerson456 <hngtraeders@gmail.com>
2025-09-05 14:05:41 -0500

    versiones3

---
+++ scripts.js
@@ -0,0 +1,20 @@
+function menus(){
+    let element = document.getElementById("activacion-menus")
+    element.classList.toggle("menu-movil")
+}
+function nav(){
+    let element = document.getElementById("nav-fixed")
+    element.classList.toggle("menu-nav")
+}
+function mostrasmas(){
+    let element = document.getElementById("mostrar-p")
+    element.classList.toggle("height-p")
+}
+function bloqbody(){
+    let element = document.getElementById("bloq-hiden")
+    element.classList.toggle("class-bloq")
+}
+function bloqmain(){
+    let element = document.getElementById("main-bloq")
+    element.classList.toggle("class-main")
+}
